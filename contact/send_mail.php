b7aa9073ecf762137622079c11620fc073945ba2
hhhdev <fastafa12@yandex.com>
2017-01-23 22:14:14 +0800

    s

--- contact/send_mail.php
+++ contact/send_mail.php
@@ -1,17 +1,62 @@
 <?php
-$name = trim($_POST['name']);
-$email = trim($_POST['email']);
-$message = trim($_POST['message']);
-$subject = 'Message from your website. '.trim($_POST['subject']);
+require 'PHPMailer/PHPMailerAutoload.php';
 
-$emailTo = 'info@trackatoo.net'; //Put your own email address here
+$host = 'premium9.web-hosting.com';
 
-$body = "Name: $name \n\nEmail: $email \n\nMessage:\n$message";
-$headers = 'From: '.$email."\r\n" .
-    'Reply-To: '.$email."\r\n";
+$username = 'info@trackatoo.net';
 
-mail($emailTo, $subject, $body, $headers);
+$password = 'Visitenkarte456';
 
-$emailSent['result'] = true;
-echo json_encode($emailSent);
+$subject = 'Request from website: ' . addslashes(strip_tags($_POST['subject']));
+
+$name = addslashes(strip_tags($_POST['name']));
+
+$email = addslashes(strip_tags($_POST['email']));
+
+$message = addslashes(strip_tags($_POST['message']));
+
+$htmlmessage = <<<MESSAGE
+    <html>
+    	<head>
+            <title>$subject</title>
+    	</head>
+        
+        <body>
+            <p><strong>Name: </strong>$name</p>
+            <p><strong>Email: </strong>$email</p>
+            <p><strong>Message: </strong>$message</p>
+        </body>
+    </html>
+MESSAGE;
+
+$mail = new PHPMailer;
+        
+$mail->isSMTP();
+$mail->SMTPAuth = TRUE;
+$mail->Host = $host;
+$mail->Username = $username;
+$mail->Password = $password;
+
+$mail->From = $email;
+$mail->FromName = $name;
+
+// Add receive email address
+$mail->addAddress($username);
+
+$mail->isHTML(true);
+
+$mail->Subject = $subject;
+
+$mail->Body    = $htmlmessage;
+
+//send the message, check for errors
+if ( $mail->send()) 
+{
+    $send['result'] = true;
+} else {
+	$send['result'] = false;
+}
+
+echo json_encode($send);
+
 ?>
